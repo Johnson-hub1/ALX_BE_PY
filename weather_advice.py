8dc95a402b920bf532731680cf7c6aa0d18aaf78
Johnson Flomo <flomojohnson506@gmail.com>
2025-06-09 02:03:22 -0700

    Add files via upload

---
+++ weather_advice.py
@@ -0,0 +1,11 @@
+weather
+weaher iput "what's the weather like your area":
+    if sunny
+        print "Wear a t-shirt and sunglasses."
+    elif "rainy":
+        Print "Don't forget your umbrella and a raincoat."
+    elif "cold":
+        print "Make sure to wear a warm coat and a scarf."
+    else
+        print "Sorry, I don't have recommendations for this weather."
+        
